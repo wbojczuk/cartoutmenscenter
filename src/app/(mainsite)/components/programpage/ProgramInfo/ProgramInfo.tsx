3f796bd00c1b8617f81f887e17627f59b7ef3be5
William Bojczuk <williambojczuk@gmail.com>
2024-01-01 09:25:23 -0500

    Changed wording

--- src/app/(mainsite)/components/programpage/ProgramInfo/ProgramInfo.tsx
+++ src/app/(mainsite)/components/programpage/ProgramInfo/ProgramInfo.tsx
@@ -8,7 +8,7 @@ export default function ProgramInfo() {
         
         <h1>Our Program</h1>
 
-        <p>To help guide these men into becoming productive members of the community and helping establish guidelines to help them manage their lives, homes, and everyday relationships that are built on a foundation of biblical principles and on the Lord Jesus Christ. <br /><br /> Our desire is to help these men with their mental, physical and spiritual needs, by providing them the opportunity to change their lives by having a personal relationship with Jesus Christ as their Lord and Savior.
+        <p>To help guide these men into becoming productive members of the community and helping establish guidelines to help them manage their lives, homes, and everyday relationships that are built on a foundation of Biblical principles and on the Lord Jesus Christ. <br /><br /> Our desire is to help these men with their mental, physical and spiritual needs, by providing them the opportunity to change their lives by having a personal relationship with Jesus Christ as their Lord and Savior.
         </p>
 
         <div style={{backgroundColor: "rgba(255,255,255,0.8)"}} className="shader"></div>
