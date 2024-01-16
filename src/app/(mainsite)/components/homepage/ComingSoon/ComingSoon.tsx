adbc833123ed1c1758c42776b664984416e5ddf0
William Bojczuk <williambojczuk@gmail.com>
2024-01-16 02:30:26 -0500

    Added coming soon msg

---
+++ src/app/(mainsite)/components/homepage/ComingSoon/ComingSoon.tsx
@@ -0,0 +1,7 @@
+import styles from "./comingsoon.module.css"
+
+export default function ComingSoon(){
+    return(
+        <h1 className={styles.comingSoon}>Coming In February 2024</h1>
+    )
+}
--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -2,11 +2,13 @@ import Header from "@/app/(mainsite)/components/homepage/Header/Header"
 import OurMission from "@/app/(mainsite)/components/homepage/OurMission/OurMission"
 import Support from "@/app/(mainsite)/components/homepage/Support/Support"
 import MediaCoverage from "@/app/(mainsite)/components/homepage/MediaCoverage/MediaCoverage"
+import ComingSoon from "@/app/(mainsite)/components/homepage/ComingSoon/ComingSoon"
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ComingSoon />
       <OurMission />
       <Support />
       <MediaCoverage />
