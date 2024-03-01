f50313bdb5851d8737b72f7c2d50cd428b0bf74d
William Bojczuk <williambojczuk@gmail.com>
2024-03-01 10:13:02 +0200

    updated

--- src/app/(mainsite)/components/homepage/ComingSoon/ComingSoon.tsx
+++ src/app/(mainsite)/components/homepage/ComingSoon/ComingSoon.tsx
@@ -2,6 +2,6 @@ import styles from "./comingsoon.module.css"
 
 export default function ComingSoon(){
     return(
-        <h1 className={styles.comingSoon}>Coming In February 2024</h1>
+        <h1 className={styles.comingSoon}>Now Open!</h1>
     )
 }
