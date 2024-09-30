45f8d7d6d33994d73ce86eaf6f2a3691d1237037
William Bojczuk <williambojczuk@gmail.com>
2024-09-29 22:50:16 -0400

    updated

--- src/app/(mainsite)/components/homepage/ComeSupport/ComeSupport.tsx
+++ src/app/(mainsite)/components/homepage/ComeSupport/ComeSupport.tsx
@@ -5,14 +5,6 @@ import { InlineIcon } from '@iconify/react';
 
 export default function ComeSupport(){
 return (
- <div className={styles.comeSupport}>
-    <h2>Come support Cartersville Outreach Men's Center!</h2>
 
-    <div className="center">
-    <Link target="_blank" href="https://cartersvilleoutreachwomenscenter.org/newsletter/#menscenter" className="main-cta">Learn More <InlineIcon icon="basil:arrow-right-outline"/> </Link>
-    </div>
-
-    <img src='/img/comesupport.webp' alt='Image of fundraiser' className='bg-img' />
-    <div className='shader' style={{backgroundColor: "rgba(0,0,0,0.7)"}}></div>
- </div>
+    <img src='/img/comesupport.webp' alt='Image of fundraiser' className={`bg-img ${styles.comeSupport}`} />
 )};
