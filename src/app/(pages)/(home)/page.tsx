b2e46e03c29bc8f54803a2d2f4ad069375a489ac
William Bojczuk <williambojczuk@gmail.com>
2024-10-09 21:42:46 -0400

    updated

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <ComeSupport />
+      {/* <ComeSupport /> */}
       <OurMission />
       <Support />
       <MediaCoverage />
