29985aa87c11e1802cf4d405d31aea8387d3302d
William Bojczuk <williambojczuk@gmail.com>
2025-03-20 20:08:54 -0400

    update

--- src/app/(mainsite)/components/testimoniespage/Testimonies/Testimonies.tsx
+++ src/app/(mainsite)/components/testimoniespage/Testimonies/Testimonies.tsx
@@ -7,6 +7,33 @@ return (
     <div className="center"><h1>Testimonies</h1></div>
 
     <div className={styles.testimony}>
+        <img src="/img/marion.webp" aria-hidden />
+        <h3>Marion Hensley</h3>
+        <p>Came in: June 28, 2025 <br />
+Graduated (In-House) Program: February 28, 2025 <br />
+Entered Transitional Phase: March 03, 2025 <br />
+
+<br /> <br />
+<strong>Galatians 2:20</strong>
+<em>I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.</em>
+<br /><br />
+
+I started out at a young age smoking weed and drinking, which led to pills, acid, hash, etc.
+I played in or created bands and we would get high to play and played to get high.
+I started to meet, what I thought was the right people, which turned out to be the wrong people, and was able to buy or be fronted large quantities of meth or weed, which I started to sell for a profit. This become my job and allowed me to have my band over all the time practicing.<br />
+At one point, I was clearing about $5,000 a week profit and was able to pay band members bills. I was able to purchase vehicles for them, as well. Early on, I was making a lot of money. I eventually got busted and had to start working and learning many trades. However, I never made more than approximately $40,000 a year.
+I got a job with a company and could utilize all of those trades on the same job. This led from making approximately $40,000 a year to approximately $160,000 a year, seemed like overnight.<br />
+After a few years, I started my own business and my pay increased to approximately $400,000 a year. The amount of money I was making led to me being able to afford almost anything that I ever wanted. <br />
+One day someone had some heroin, so I asked for a line, when they gave it to me they told me that it would take everything that I had and it did!!! I was on heroin 3 years, but it broke me mentally and wrecked my body physically.
+After going to a methadone clinic, on four separate occasions, to get help, and going back and doing heroin again and again, I met these Pastors and was told to complete an application for Cartersville Outreach Men's Center. 
+<br /><br />
+I completed the application and was accepted into their program, where I completed the 8 month, in house program. During this time, I found Jesus Christ and was allowed to close all my addiction doors. Now, the high of The Holy Spirit is overwhelming, with prayers, Bible studies and isolation from all of my old running buddies, Cartersville Outreach Men's Center's strict policies has allowed me to sustain my sobriety, which I plan to continue when I leave the program completely.
+<br /><br />
+<strong>Jeremiah 29:11</strong> -
+<em>For I know the plans I have for you, declares the Lord, plans to prosper you and not harm you, plans to give you hope and a future.</em></p>
+    </div>
+
+    <div className={styles.testimony}>
         <img src="/img/joe.webp" aria-hidden />
         <h3>Joe Cromer</h3>
         <p>This place has changed my life. I've been saturated in the word learning who God is and who I am in Christ. The love has been so real and I feel like I am part of the family of the church. I am so thankful that the ministry has given me purpose and a reason for me to move forward in my life. Showed me how to love and be loved. I have been blessed with a truck and a job. God is real and He loves me. Jesus is the Way the Truth and Life. Just let Him direct your path. Thank you Cartersville Outreach as a whole for being obedient to God.</p>
