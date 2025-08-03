4a3dd8d881d99676012b4938756942448caffa20
William Bojczuk <williambojczuk@gmail.com>
2025-08-03 15:21:49 -0400

    upodated

--- src/app/(mainsite)/components/homepage/ComeSupport/ComeSupport.tsx
+++ src/app/(mainsite)/components/homepage/ComeSupport/ComeSupport.tsx
@@ -4,5 +4,5 @@ import styles from './comesupport.module.css';
 export default function ComeSupport(){
 return (
 
-    <img src='/img/tentrevival.webp' alt='Image of revival' className={`bg-img ${styles.comeSupport}`} />
+    <img src='/img/budgetel.webp' alt='Image of revival' className={`bg-img ${styles.comeSupport}`} />
 )};
