4f0bda945e8db395ce843ebd7ea4c0519ac82caf
William Bojczuk <williambojczuk@gmail.com>
2025-10-28 21:02:48 -0400

    he

--- src/app/(pages)/(home)/page.tsx
+++ src/app/(pages)/(home)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Header />
-      <ComeSupport />
+      {/* <ComeSupport /> */}
       <Photo />
       <OurMission />
       <MediaCoverage />
